d7096a832c6383a9aa69ad3d9c82478fa1559b65
“Sanduni <sanduni_imalka@yahoo.com>
2024-01-15 00:59:24 +0530

    “todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { TodoscomComponent } from './todoscom/todoscom.component';
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TodoscomComponent],
+  imports: [CommonModule, RouterOutlet , TodoscomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/todoscom/todoscom.component.ts
+++ src/app/todoscom/todoscom.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todoscom',
@@ -10,4 +10,6 @@ import { Component } from '@angular/core';
 })
 export class TodoscomComponent {
 
+
 }
+
