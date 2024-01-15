bdcba62233954fa93657ae2f3dc241be5fa6f5c4
“Sanduni <sanduni_imalka@yahoo.com>
2024-01-15 18:21:43 +0530

    todocomponent part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TodoscomComponent } from './todoscom/todoscom.component';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule, RouterOutlet , TodoscomComponent],
+  imports: [CommonModule, RouterOutlet , TodoscomComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+
 
 export class AppComponent {
   title = 'todolist';
--- src/app/todoscom/todoscom.component.ts
+++ src/app/todoscom/todoscom.component.ts
@@ -1,15 +1,46 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../class/todo';
+import { FormsModule, NgModel } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-todoscom',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todoscom.component.html',
   styleUrl: './todoscom.component.css'
 })
 export class TodoscomComponent {
+todoValue: String = '';
 
+todoList: Todo[] = [
+  {
+    content: "Todo 1",
+    value: false
+  },
+  {
+    content: "Todo 2",
+    value: false
+  },
+  {
+    content: "Todo 3",
+    value: false
+  }
+];
+finishedList: Todo[] = [
 
+]
+constructor(){}
+
+addTodo(){
+this.todoList.push({content:this.todoValue, value:false});
+this.todoValue ='';
+}
+changeTodo(i: number){
+const item = this.todoList.splice(i,1);
+console.log(item);
+this.finishedList.push(item[0]);
+}
 }
 
